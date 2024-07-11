b9ab8f624f0d9b125efc18c203603a98653b4acf
imasretep <samipetersalmi@gmail.com>
2024-07-11 21:14:47 +0300

    uibuild

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const Person = require('./models/person')
-var morgan = require('morgan')
+const morgan = require('morgan')
 const cors = require('cors')
 const app = express()
 
